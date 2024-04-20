dd88f245bf7ca24ed8596b1630bfa778ecd43523
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-04-20 12:10:56 +0530

    Dashboard UI and signOut functionality done

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,77 @@
+import authOptions from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import SignOutButton from "@/components/SignOutButton";
+interface LayoutProps{
+    children: React.ReactNode
+}
+
+interface SideBarOption {
+    id: number,
+    name: string,
+    href: string,
+}
+
+const Layout:React.FC<LayoutProps> = async ({children}) => {
+
+    const session = await getServerSession(authOptions);
+    if(!session){notFound()}
+    const sidebarOptions: SideBarOption[] = [
+        {
+            id: 1,
+            name: 'Add friend',
+            href: '/dsahboard/add'
+        }
+    ]
+    return (  
+        <div className="w-full flex h-screen">
+            <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto  bg-gray-700 px-6">
+                <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+                    Logo
+                </Link>
+                <div className="text-xs font-semibold leading-6 text-gray-400">Your chats</div>
+                <nav className="flex flex-1 flex-col">
+                    <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                        <li>
+                            User chats
+                        </li>
+                        <li>
+                            <div className="text-xs font-semibold leading-6 text-gray-400">
+                                Overview
+                            </div>
+                            <ul role="list" className="-mx-2 mt-2 space-y-1">
+                                {sidebarOptions.map((option) => {
+                                    return (
+                                        <li key={option.id}>
+                                            <Link href={option.href} className="text-white hover:text-indigo-600">
+                                                <span className="truncate">{option.name}</span>
+                                            </Link>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </li>
+                        <li className="-mx-6 mt-auto flex items-center">
+                                <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white">
+                                    <div className="relative h-8 w-8 bg-gray-100">
+                                        <Image fill referrerPolicy="no-referrer" className="rounded-full" src={session.user.image || ""} alt="your profile image"/>
+                                    </div>
+                                    <span className="sr-only text-white">Your profile</span>
+                                    <div className="flex flex-col">
+                                        <span aria-hidden='true'>{session.user.name}</span>
+                                        <span className="text-xs text-zinc-400" aria-hidden='true'>{session.user.email}</span>
+                                    </div>
+                                </div>
+                                <SignOutButton className="h-full aspect-square"/>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            {children}
+        </div>
+    );
+}
+ 
+export default Layout;
---
+++ components/SignOutButton.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { signOut } from "next-auth/react";
+import { ButtonHTMLAttributes, useState } from "react";
+import toast from "react-hot-toast";
+interface SignOutButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+
+}
+
+
+const SignOutButton:React.FC<SignOutButtonProps> = ({...props}) => {
+    
+    const [isSignOut, setIsSignOut] = useState<boolean>(false);
+    return (  
+        <button {...props} onClick={async () => {setIsSignOut(true);
+            try {
+                await signOut();
+            } catch (error) {
+                toast.error('There was a problem signing out')
+            } finally {
+                setIsSignOut(false);
+            }
+        }}>
+            {isSignOut ? (
+                <>Loading...</>
+            ): (
+                <>SignOut</>
+            )}
+
+        </button>
+    );
+}
+ 
+export default SignOutButton;
