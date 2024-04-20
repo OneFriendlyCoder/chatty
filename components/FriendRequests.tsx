c705d0c54d8b211458b3bd215754c839d52538f0
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-04-20 19:31:56 +0530

    sending friend requests done

--- components/FriendRequests.tsx
+++ components/FriendRequests.tsx
@@ -12,13 +12,13 @@ const FriendRequests:React.FC<IncomingFriendRequestProps> = ({incomingFriendRequ
     const router = useRouter();
     
     const acceptFriend = async (senderId: string) => {
-        await axios.post('/api/requests/accept', {id: senderId})
+        await axios.post('/api/friends/accept', {id: senderId})
         setFriendRequests(prev => prev.filter(request => request.senderId !== senderId))
         router.refresh();
     }
 
     const denyFriend = async (senderId: string) => {
-        await axios.post('/api/requests/deny', {id: senderId})
+        await axios.post('/api/friends/deny', {id: senderId})
         setFriendRequests(prev => prev.filter(request => request.senderId !== senderId))
         router.refresh();
     }
@@ -34,8 +34,8 @@ const FriendRequests:React.FC<IncomingFriendRequestProps> = ({incomingFriendRequ
                     <div key={request.senderId} className="flex gap-4 items-center">
                         <span>UserIcon</span>
                         <p className="font-medium text-lg">{request.senderEmail}</p>
-                        <button aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-800 grid place-items-center rounded-full"></button>
-                        <button aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-800 grid place-items-center rounded-full"></button>
+                        <button onClick={() => {acceptFriend(request.senderId)}} aria-label="accept friend" className="w-8 h-8 bg-indigo-600 hover:bg-indigo-800 grid place-items-center rounded-full"></button>
+                        <button onClick={() => {denyFriend(request.senderId)}} aria-label="deny friend" className="w-8 h-8 bg-red-600 hover:bg-red-800 grid place-items-center rounded-full"></button>
                     </div>
                 ))
             )
